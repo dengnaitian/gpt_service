70e38838ebe6b847a3b374dc2e86cfa91a7fa0fe
dengnaitian <756202225@qq.com>
2022-12-29 00:05:38 +0800

    [modify] 添加日志打印 [user] dengnaitian

--- src/main/java/com/example/swagger/service/UserDelegateImpl.java
+++ src/main/java/com/example/swagger/service/UserDelegateImpl.java
@@ -3,11 +3,13 @@ package com.example.swagger.service;
 import com.example.swagger.api.pojo.Response;
 import com.example.swagger.api.pojo.User;
 import com.example.swagger.rest.UserApiDelegate;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class UserDelegateImpl implements UserApiDelegate {
 
     @Override
@@ -17,6 +19,7 @@ public class UserDelegateImpl implements UserApiDelegate {
         response.setCode("success");
         response.setMessage("successMessage");
         ResponseEntity<Response> responseEntity = new ResponseEntity<>(response, HttpStatus.OK);
+        log.info("begin saveUser");
         return responseEntity;
     }
 }
